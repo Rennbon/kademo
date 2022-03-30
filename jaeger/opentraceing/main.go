f5f336db9902b5793002f691602fd0be48641fc8
Rennbon <343688972@qq.com>
2022-03-30 23:38:01 +0800

    no message

--- jaeger/opentraceing/main.go
+++ jaeger/opentraceing/main.go
@@ -13,13 +13,25 @@ import (
 	"unsafe"
 )
 
+/*docker run -d --name jaeger \
+-e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
+-p 5775:5775/udp \
+-p 6831:6831/udp \
+-p 6832:6832/udp \
+-p 5778:5778 \
+-p 16686:16686 \
+-p 14250:14250 \
+-p 14268:14268 \
+-p 14269:14269 \
+-p 9411:9411 \
+jaegertracing/all-in-one:1.32*/
 func InitJaeger() (opentracing.Tracer, io.Closer) {
 	cfg := config.Configuration{
 		Sampler: &config.SamplerConfig{
 			Type:  jaeger.SamplerTypeConst,
 			Param: 1,
 		},
-		ServiceName: "kademo",
+		ServiceName: "kademo1",
 		Reporter: &config.ReporterConfig{
 			LocalAgentHostPort: "192.168.1.30:6831",
 			LogSpans:           true,
@@ -27,7 +39,7 @@ func InitJaeger() (opentracing.Tracer, io.Closer) {
 	}
 	jLogger := jaegerlog.StdLogger
 	jMetricsFactory := metrics.NullFactory
-	closer, err := cfg.InitGlobalTracer("kademo", config.Logger(jLogger), config.Metrics(jMetricsFactory))
+	closer, err := cfg.InitGlobalTracer("kademo1", config.Logger(jLogger), config.Metrics(jMetricsFactory))
 	if err != nil {
 		panic(err)
 	}
@@ -43,7 +55,7 @@ func main() {
 	spanId, _ := jaeger.SpanIDFromString("56fad8b70e01af11")
 	pid, _ := jaeger.SpanIDFromString("752aa5d597d7c7e0")
 	spanCtx := jaeger.NewSpanContext(traceId, spanId, pid, true, nil)
-	sp := tr.StartSpan("test2")
+	sp := tr.StartSpan("test3")
 	spp := sp.(*jaeger.Span)
 	ri := reflect.ValueOf(sp).Elem()
 
